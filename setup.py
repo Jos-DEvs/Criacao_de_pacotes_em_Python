12e817bcae13628731bc0297166e8d729fa57943
Jos-DEvs <105674636+Jos-DEvs@users.noreply.github.com>
2024-12-28 16:12:01 -0300

    setup.py

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ with open("requirements.txt") as f:
 setup(
     name="package_name",
     version="0.0.1",
-    author="my_name",
+    author="Jos-DEvs",
     author_email="my_email",
-    description="My short description",
+    description="Testar a acessibilidade de um site",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/Jos-DEvs/Criacao_de_pacotes_em_Python.git"
     packages=find_packages(),
     install_requires=requirements,
-    python_requires='>=3.8',
+    python_requires='>=3.12.2',
 )
